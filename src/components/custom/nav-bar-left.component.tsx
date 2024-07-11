56abb9ce7d4293450d18f13d7cf3df385cca8917
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2024-07-10 22:24:04 -0300

    add css to make left nav look pretty

--- src/components/custom/nav-bar-left.component.tsx
+++ src/components/custom/nav-bar-left.component.tsx
@@ -20,24 +20,27 @@ const navItems = [
 export function NavBarLeftComponent() {
   const pathname = usePathname();
   return (
-    <div className="dark:bg-black hidden lg:block border-r border-neutral-300 h-full bg-white text-black w-72">
+    <div className="hidden lg:block border-r border-neutral-300 h-full bg-white  text-black w-72">
       <div className="border-neutral-300 flex h-14 items-center justify-between border-b px-3 w-full">
         <Link className="font-semibold" href={RoutesEnum.HOME}>
           Nextjs Starter Kit
         </Link>
       </div>
       <nav className="px-4 font-medium text-sm py-4">
-        <ul className="flex flex-col gap-4">
+        <ul className="flex flex-col">
           {navItems.map((item) => {
             return (
               <li
                 key={item.name}
-                className={clsx("text-gray-500 hover:text-gray-900", {
-                  "text-gray-900": item.href === pathname,
-                })}
+                className={clsx(
+                  "text-gray-500 hover:text-gray-900 rounded-lg py-2 px-3",
+                  {
+                    "text-gray-900 bg-gray-100": item.href === pathname,
+                  },
+                )}
               >
-                <Link className="flex flex-row" href={item.href}>
-                  <div className="border rounded-lg dark:bg-black dark:border-gray-800 border-gray-400 p-1 bg-white">
+                <Link className="flex flex-row gap-2" href={item.href}>
+                  <div className="border rounded-lg border-gray-400 p-1 bg-inherit">
                     {item.icon}
                   </div>
                   {item.name}
